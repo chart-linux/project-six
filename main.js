69631962b22f258eef3d97794ad1afc2772a0e78
Y.Sangenya <longinus.eva@gmail.com>
2014-08-14 01:24:34 +0900

    faviconの追加、タイトルの設定、大きな写真の後にできる空白をなくした。

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 ($(function(){
-    var json = '{"ids":[{"id":"001","num":4},{"id":"002","num":19},{"id":"003","num":1},{"id":"004","num":206},{"id":"005","num":436},{"id":"006","num":671},{"id":"007","num":147},{"id":"008","num":17},{"id":"009","num":6},{"id":"010","num":6},{"id":"011","num":2},{"id":"012","num":12},{"id":"013","num":7},{"id":"014","num":9}]}';
+    var json = '{"ids":[{"id":"001","num":4},{"id":"002","num":19},{"id":"003","num":1},{"id":"004","num":206},{"id":"005","num":436},{"id":"006","num":671},{"id":"007","num":147},{"id":"008","num":17},{"id":"009","num":6},{"id":"010","num":6},{"id":"011","num":2},{"id":"012","num":12},{"id":"013","num":7},{"id":"014","num":9},{"id":"015","num":50},{"id":"016","num":90},{"id":"017","num":14},{"id":"018","num":87},{"id":"019","num":400},{"id":"020","num":39}]}';
     var radius = 300.0;
     var list = [];
     var show_list;
@@ -18,9 +18,24 @@
     var _sum = 0;
     var Pos = [];
     
-    for(var i=1; i <= 14; i++){
+    //// animation /////
+    for(var i=1; i <= 20; i++){
         list.push("#_" + ("00" + String(i)).slice(-3));
     }
+    list.forEach(function(slctr){
+        $(slctr).mouseover(function(){
+            $(this).stop().animate(
+                {"opacity": 1.0},
+                {"duration":"fast"," easing": "easeInCirc","queue": false}
+            );
+        });
+        $(slctr).mouseout(function(){
+            $(this).stop().animate(
+                {"opacity": 0.6},
+                {"duration":"fast"," easing": "easeInCirc","queue": false}
+            );
+        });
+    });
     
     //// calculate theta [{id,num}] -> [[x,y]] ////
     
@@ -34,14 +49,14 @@
         }
     });
     temp.forEach(function(elem, index){
-        T.push(elem/max)
+        T.push(parseFloat(elem)/parseFloat(max))
     });
     
     //// Tominaga Writes ////
     
     ////length////
     for(var i=0; i<T.length; i++){
-        L.push(200.0*T[i]+100.0);
+        L.push(150.0*T[i]+75.0);
     }   
     
     ///sum///
@@ -70,20 +85,21 @@
             L[i]*1.1]);
         }else{
             __thete=2.0*(_thete[i]-(Math.PI+omega));
-            Pos.push([ radius + 1.0/2.0*radius*Math.sin(__thete), radius*3.0/2.0 + 1/2.0*radius*Math.cos(__thete) , L[i]*1.1]);
-    }}
-
+            Pos.push([ radius + 1.0/2.0*radius*Math.sin(__thete), radius*3.0/2.0 + 1/2.0*radius*Math.cos(__thete) , L[i]*1.2]);
+        }
+    }
+    
     //// Tominaga Wrote /////
     for(var i = 0; i < Pos.length; i++){
         dict = dict.concat([["#_" + ("00"+String(i + 1)).slice(-3)].concat(Pos[i])]);
     }
     
-    dict.forEach(function(elem){
-        setPict(elem[0],elem[1],elem[2],elem[3]);
+    dict.reverse().forEach(function(elem,index){
+        setPict(elem[0],elem[1],elem[2],elem[3],index);
     });
     
     ///// Definitioin of Functions //////////
-    function setPict(selector,x,y,r){
-        $(selector).css("left",x-r).css("top",y-r).css("width",r).css("height",r).css("border-radius",r/2.0);
+    function setPict(selector,x,y,r,layer){
+        $(selector).css("left",x-r/2.0).css("top",y-r/2.0).css("width",r).css("height",r).css("border-radius",r/2.0).css("z-index",layer);
     }
 }))

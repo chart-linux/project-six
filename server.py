f4ca9e32775506ffb6a251c0e917138f6ce89d75
Y.Sangenya <longinus.eva@gmail.com>
2014-08-15 02:28:56 +0900

    base-authentification

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
-from bottle import Bottle, run, static_file
+from bottle import Bottle, run, static_file, auth_basic
 import sys
+
+USERNAME = "user"
+PASSWORD = "pass"
 
 app = Bottle()
 
+def check(username, password):
+    if username == USERNAME and password == PASSWORD:
+        return True
+    else:
+        return False
+
 @app.route("/")
+@auth_basic(check)
 def six():
     return open("./static/index.html").read()
 
 @app.route("/static/<filename:path>")
+@auth_basic(check)
 def static_dir(filename):
     return static_file(filename, root="./static")
 

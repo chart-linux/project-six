0b06202cc3fe6fe012da5e5054774dc55939ab3b
Y.Sangenya <longinus.eva@gmail.com>
2014-08-14 17:04:59 +0900

    Procfile, etc...

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from bottle import Bottle, run, static_file
+import sys
 
 app = Bottle()
 
 @app.route("/")
 def six():
-    return open("/static/html/index.html").read()
+    return open("./static/index.html").read()
 
 @app.route("/static/<filename:path>")
 def static_dir(filename):
     return static_file(filename, root="./static")
 
-run(app, host="localhost", port=8080)
+run(app, host="0.0.0.0", port=int(sys.argv[1]))

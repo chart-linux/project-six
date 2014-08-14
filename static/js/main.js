f58680555b5bde862a6bb2ef65f0b7b54f161b58
Y.Sangenya <longinus.eva@gmail.com>
2014-08-15 00:17:22 +0900

    画像を、本番画像に差し替えるにあたって、各スクリプトを更新した。なお、このレポジトリは、github上で公開しているので、肖像権等の問題を考え、画像はgitに登録しないことにした。

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,4 @@
 ($(function(){
-    var json = '{"ids":[{"id":"001","num":4},{"id":"002","num":19},{"id":"003","num":1},{"id":"004","num":206},{"id":"005","num":436},{"id":"006","num":671},{"id":"007","num":147},{"id":"008","num":17},{"id":"009","num":6},{"id":"010","num":6},{"id":"011","num":2},{"id":"012","num":12},{"id":"013","num":7},{"id":"014","num":9},{"id":"015","num":50},{"id":"016","num":90},{"id":"017","num":14},{"id":"018","num":87},{"id":"019","num":400},{"id":"020","num":39}]}';
     var radius = 300.0;
     var list = [];
     var show_list;
@@ -17,41 +16,40 @@
     var sum = 0;
     var _sum = 0;
     var Pos = [];
+    var Tmax;
+
+    $.ajaxSetup({async: false});
     
-    //// animation /////
-    for(var i=1; i <= 20; i++){
-        list.push("#_" + ("00" + String(i)).slice(-3));
-    }
-    list.forEach(function(slctr){
-        $(slctr).mouseover(function(){
+    $.getJSON("/static/js/data.js", function(temp_json){
+        var json = temp_json.reverse();
+        Tmax = json.map(function(data){ return data.num; }).reduce(function(prev,curr){
+            if(prev > curr){
+                return prev;
+            } else { 
+                return curr;
+            }
+        });
+
+        json.forEach(function(data,index,arr){
+        $("div#menu").append("<img src=\"" + data["file_name"] + "\" id=\"" + data["id"] + "\" />");
+        $("#" + data.id).css("z-index", arr.length - 1 - index);
+        $("#" + data["id"]).mouseover(function(){
             $(this).stop().animate(
                 {"opacity": 1.0},
                 {"duration":"fast"," easing": "easeInCirc","queue": false}
             );
         });
-        $(slctr).mouseout(function(){
+        $("#" + data.id).mouseout(function(){
             $(this).stop().animate(
                 {"opacity": 0.6},
                 {"duration":"fast"," easing": "easeInCirc","queue": false}
             );
         });
+        T.push(parseFloat(data.num)/parseFloat(Tmax));
+        });
     });
     
-    //// calculate theta [{id,num}] -> [[x,y]] ////
-    
-    ///// gen T /////
-    var temp = $.parseJSON(json)["ids"].map(function(elem){return parseFloat(elem["num"])});
-    var max = temp.reduce(function(prev,curr,index,array){
-        if(prev > curr){
-            return prev;
-        }else{
-            return curr;
-        }
-    });
-    temp.forEach(function(elem, index){
-        T.push(parseFloat(elem)/parseFloat(max))
-    });
-    
+    //// calculate position ////
     //// Tominaga Writes ////
     
     ////length////
@@ -82,24 +80,24 @@
             Pos.push([
             radius - radius*Math.sin(_thete[i]-omega) ,
             radius - radius*Math.cos(_thete[i]-omega) ,
-            L[i]*1.1]);
+            L[i]*0.9]);
         }else{
             __thete=2.0*(_thete[i]-(Math.PI+omega));
-            Pos.push([ radius + 1.0/2.0*radius*Math.sin(__thete), radius*3.0/2.0 + 1/2.0*radius*Math.cos(__thete) , L[i]*1.2]);
+            Pos.push([ radius + 1.0/2.0*radius*Math.sin(__thete), radius*3.0/2.0 + 1/2.0*radius*Math.cos(__thete) , L[i]*0.9]);
         }
     }
     
     //// Tominaga Wrote /////
     for(var i = 0; i < Pos.length; i++){
-        dict = dict.concat([["#_" + ("00"+String(i + 1)).slice(-3)].concat(Pos[i])]);
+        dict = dict.concat([["#_" + ("00"+String(Pos.length -1 - i)).slice(-3)].concat(Pos[i])]);
     }
     
-    dict.reverse().forEach(function(elem,index){
-        setPict(elem[0],elem[1],elem[2],elem[3],index);
+    dict.forEach(function(elem){
+        setPict(elem[0],elem[1],elem[2],elem[3]);
     });
     
     ///// Definitioin of Functions //////////
-    function setPict(selector,x,y,r,layer){
-        $(selector).css("left",x-r/2.0).css("top",y-r/2.0).css("width",r).css("height",r).css("border-radius",r/2.0).css("z-index",layer);
+    function setPict(selector,x,y,r){
+        $(selector).css("left",x-r/2.0).css("top",y-r/2.0).css("width",r).css("height",r).css("border-radius",r/2.0);
     }
 }))
